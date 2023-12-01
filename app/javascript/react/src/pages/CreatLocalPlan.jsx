2f0fbfbb3cac93c8bb56a71fe302720e902afb14
Thiago <thiagocrrabelo@gmail.com>
2023-12-01 00:00:12 -0300

    CreateLocalPlan (wip): create page to add local plan

--- app/javascript/react/src/pages/CreatLocalPlan.jsx
+++ app/javascript/react/src/pages/CreatLocalPlan.jsx
@@ -1,28 +1,28 @@
-import React, { useState } from 'react';
-import Inputs from '../components/Inputs';
-import styled from 'styled-components';
+import React, { useState } from "react";
+import Inputs from "../components/Inputs";
+import styled from "styled-components";
 
 const App = () => {
-  const [steps, setSteps] = useState([{ skill: '', parameters: '', goal: '' }]);
+  const [steps, setSteps] = useState([{ skill: "", parameters: "", goal: "" }]);
 
   const handleChange = (index, name, value) => {
-      const newSteps = [...steps];
-      newSteps[index][name] = value;
-      setSteps(newSteps);
+    const newSteps = [...steps];
+    newSteps[index][name] = value;
+    setSteps(newSteps);
   };
 
   const handleAddStep = () => {
-      setSteps([...steps, { skill: '', parameters: '', goal: '' }]);
+    setSteps([...steps, { skill: "", parameters: "", goal: "" }]);
   };
 
   const handleRemoveStep = (index) => {
-      const newSteps = steps.filter((_, i) => i !== index);
-      setSteps(newSteps);
+    const newSteps = steps.filter((_, i) => i !== index);
+    setSteps(newSteps);
   };
 
   const handleDeletePlan = (index) => {
-    setSteps([])
-};
+    setSteps([]);
+  };
 
   const TitleDiv = styled.div`
     display: flex;
@@ -30,13 +30,11 @@ const App = () => {
     justify-content: space-between;
     padding: 0 1rem;
   `;
-
   const ContentDiv = styled.div`
     display: grid;
     grid-template-columns: 1fr;
     gap: 1rem;
   `;
-
   return (
     <ContentDiv>
       <TitleDiv>
@@ -44,7 +42,7 @@ const App = () => {
         <button onClick={handleDeletePlan}>Deletar Plano Local</button>
       </TitleDiv>
       {steps.map((step, index) => (
-        <Inputs key={index} params={step} index={index} onChange={handleChange} onRemove={handleRemoveStep} />
+        <Inputs onRemove={handleRemoveStep} />
       ))}
       <button onClick={handleAddStep}>Adicionar Passo</button>
     </ContentDiv>

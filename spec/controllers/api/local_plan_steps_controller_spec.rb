7b623325769051c5b2085826b54856ecd3e499d6
Henrixii <luizhfs09@gmail.com>
2023-12-03 11:51:10 -0300

    test:fix a test

--- spec/controllers/api/local_plan_steps_controller_spec.rb
+++ spec/controllers/api/local_plan_steps_controller_spec.rb
@@ -10,12 +10,13 @@ describe Api::LocalPlanStepsController, type: :controller do
     describe '/GET #create' do
         let!(:robot) {Robot.create!()}
         let!(:local_plan_steps_params) do
-            {stepNumber:10, label:"teste", parameters: "teste", robot_id: robot.id}
+            {stepNumber:10, label:"teste", parameters: "teste", skill:"test", robot_id: robot.id}
         end
         context 'should create a plan with good params' do
             it 'return status created' do
                 get :create, params: {local_plan_steps: local_plan_steps_params}
                     expect(response).to have_http_status(:created)
+                    expect(LocalPlanStep.count).to change_by(1) 
             end
         end
         context 'should create a plan with bad params' do
@@ -26,9 +27,9 @@ describe Api::LocalPlanStepsController, type: :controller do
             end
         end
     end
-    describe 'DELETE #destroy' do
+    describe 'DELETE #delete' do
     let!(:robot) {Robot.create!()}
-    let!(:local_plan_step) { LocalPlanStep.create!(stepNumber: 1, label: 'Exemplo', parameters: 'Parâmetros', robot_id: robot.id) }
+    let!(:local_plan_step) { LocalPlanStep.create!(stepNumber: 1, label: 'Exemplo', parameters: 'Parâmetros', skill:"test", robot_id: robot.id) }
     it 'delete a local plan' do
       delete :delete, params: { id: local_plan_step.id }
       expect(response).to have_http_status(:ok)
@@ -40,28 +41,28 @@ describe Api::LocalPlanStepsController, type: :controller do
     end
   end
   
-  describe 'PUT #update' do
+  describe 'PATCH #update' do
     let!(:robot) {Robot.create!()}
-    let!(:local_plan_step) { LocalPlanStep.create!(stepNumber: 1, label: 'Exemplo', parameters: 'Parâmetros', robot_id: robot.id) }
+    let!(:local_plan_step) { LocalPlanStep.create!(stepNumber: 1, label: 'Exemplo', parameters: 'Parâmetros', skill:"test", robot_id: robot.id) }
 
-    it 'atualiza um local_plan_step' do
+    it 'Update a local plan' do
       updated_params = { label: 'Novo Label' }
 
-      put :update, params: { id: local_plan_step.id, local_plan_steps: updated_params }
+      patch :update, params: { id: local_plan_step.id, local_plan_steps: updated_params }
 
       expect(response).to have_http_status(:ok)
       expect(JSON.parse(response.body)['label']).to eq('Novo Label')
     end
 
-    it 'retorna erro se o local_plan_step não existir' do
-      put :update, params: { id: nil , local_plan_steps: { label: 'Novo Label' } }
+    it 'return a error if local_plan_step dont exist' do
+      patch :update, params: { id: nil , local_plan_steps: { label: 'Novo Label' } }
 
       expect(response).to have_http_status(:not_found)
     end
 
-    it 'retorna erro se a atualização falhar' do
+    it 'returns error if update fails' do
       allow_any_instance_of(LocalPlanStep).to receive(:update!).and_raise(ActiveRecord::RecordInvalid.new(LocalPlanStep.new))
-      put :update, params: { id: local_plan_step.id, local_plan_steps: { label: 'Novo Label' } }
+      patch :update, params: { id: local_plan_step.id, local_plan_steps: { label: 'Novo Label' } }
       expect(response).to have_http_status(:bad_request)
     end
   end

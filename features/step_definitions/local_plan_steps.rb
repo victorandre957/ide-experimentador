d6d77dc575f5c7c6ee15ae3d20e1192fa09c2c51
Arthur C <arthur_07q@hotmail.com>
2023-12-03 18:04:02 -0300

    Remove unwanted annotation

--- features/step_definitions/local_plan_steps.rb
+++ features/step_definitions/local_plan_steps.rb
@@ -1,11 +1,11 @@
 When /^fill in all the information$/ do
   fill_in 'labelInput', with: 'StepName'
   select 'Navegação', from: 'skillSelect'
-  fill_in 'Sala', with: 'RoomName' # Added a colon after 'with'
+  fill_in 'Sala', with: 'RoomName'
   click_button 'Adicionar waypoint'
-  fill_in 'x', with: 1.3  # Added a colon after 'with', and corrected the value to 1.3
-  fill_in 'y', with: 2.2  # Added a colon after 'with', and corrected the value to 2.2
-  fill_in 'z', with: 3.1  # Added a colon after 'with', and corrected the value to 3.1
+  fill_in 'x', with: 1.3
+  fill_in 'y', with: 2.2
+  fill_in 'z', with: 3.1
 end
   
 When /^click "Confirm"$/ do

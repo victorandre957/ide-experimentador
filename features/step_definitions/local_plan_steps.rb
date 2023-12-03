8a1c248445c6eab37a263368e2cbff402f20c01b
Arthur C <arthur_07q@hotmail.com>
2023-12-03 18:21:45 -0300

    Test cancelling the creation of a local plan

--- features/step_definitions/local_plan_steps.rb
+++ features/step_definitions/local_plan_steps.rb
@@ -1,3 +1,7 @@
+When /^I'm on the$/ do
+  visit /local_plan_steps/999
+end
+
 When /^fill in all the information$/ do
   fill_in 'labelInput', with: 'StepName'
   select 'Navegação', from: 'skillSelect'
@@ -10,4 +14,19 @@ end
   
 When /^click "Confirm"$/ do
   click_button 'Save'
+end
+
+When /^click "Cancel"$/ do
+  r = Robot.create!(id: 999)
+  visit '/local_plan_steps/999'
+  fill_in 'labelInput', with: 'StepName'
+  select 'Navegação', from: 'skillSelect'
+  fill_in 'Sala', with: 'RoomName'
+  click_button 'Adicionar waypoint'
+  fill_in 'x', with: 1.3
+  fill_in 'y', with: 2.2
+  fill_in 'z', with: 3.1
+  click_button 'Save'
+  click_button 'Deletar Plano Locals'
+  LocalPlanStep.count(robot_id: 999).to eq(0)
 end

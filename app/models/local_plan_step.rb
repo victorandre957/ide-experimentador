42250300bc41358444c0139497dc0fb004a7d068
GSTV-AVL <gusavila0317@gmail.com>
2023-12-03 21:44:13 -0300

    app/models/local_plan_step.rb (validação) : Finalização da validação para o plano local, formatado

--- app/models/local_plan_step.rb
+++ app/models/local_plan_step.rb
@@ -1,43 +1,9 @@
-arraySkills = [
-  "operate_drawer",
-  "navigation",
-  "authenticate_person",
-  "approach_robot",
-  "approach_person",
-  "wait_message",
-  "send_message"]
-
 class ParameterValidator < ActiveModel::Validator
   def validate(record)
     puts record.parameter
     puts record.skill
     case record.skill
       when "navigation"
-=begin
-        "parameter":{
-          "room":"PC Room 3",
-          "waypoints": [
-              {
-                  "x":-27.23,
-                  "y":18.0,
-                  "z":-1.57
-              },
-              {
-                  "x":-27.23,
-                  "y":16.0
-              },
-              {
-                  "x":-28.5,
-                  "y":16.0
-              },
-              {
-                  "x":-28.5,
-                  "y":18.0,
-                  "z":-1.57
-              }
-          ]
-      },
-=end
         if  !(record.parameter.has_key?("room")) or !(record.parameter.has_key?("waypoints"))
           record.errors.add :base, "Incorrect parameter formating"    
 
@@ -49,21 +15,11 @@ class ParameterValidator < ActiveModel::Validator
         end
 
       when "approach_robot", "authenticate_person", "approach_person", "wait_message", "send_message"
-=begin
-        "parameter": [
-          {"topic": "nurse"}
-      ],
-=end
         if  !(record.parameter.has_key?("topic")) or record.parameter.size != 1
           record.errors.add :base, "Incorrect parameter formating"       
         end
 
-      when "operate_drawer"
-=begin
-                "parameter": [
-                  {"action": "open"}
-              ],
-=end        
+      when "operate_drawer"   
         if  !(record.parameter.has_key?("action")) or record.parameter.size != 1
           record.errors.add :base, "Incorrect parameter formating"
         elsif record.parameter["action"] != "open" or record.parameter["action"] != "close"
@@ -77,8 +33,6 @@ class ParameterValidator < ActiveModel::Validator
 end
 
 class LocalPlanStep < ApplicationRecord
-  testArray = []
-  #validates :robot_id, presence: true
   belongs_to :robot
   validates :stepNumber, presence: true, numericality: true
   validates :label, presence: true, uniqueness: { scope: :robot_id }

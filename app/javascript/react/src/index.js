5f575ce0d3e3f51e26262b2663c87a9a02df3e87
Victor André <victorandre957@gmail.com>
2023-12-14 14:44:13 -0300

    (wip): check rspec

--- app/javascript/react/src/index.js
+++ app/javascript/react/src/index.js
@@ -1,8 +1,6 @@
 import { define } from 'remount'      
-import Hello from "./components/Hello"
 import Graph from "./components/Graph"
 import CreateLocalPlan from "./pages/CreateLocalPlan";
 import { wrapWithQueryProvider } from "./wrapWithQueryProvider";
                                       
-define({ 'hello-component': Hello, 'graph-component': Graph })
 define({ "create-local-plan-component": wrapWithQueryProvider(CreateLocalPlan) });

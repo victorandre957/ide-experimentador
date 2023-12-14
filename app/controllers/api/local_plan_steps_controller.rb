15acc0e6a76030d88205d3af125950a5dfd40b64
Victor André <victorandre957@gmail.com>
2023-12-14 16:24:56 -0300

    controllers/ (docs): document controllers

--- app/controllers/api/local_plan_steps_controller.rb
+++ app/controllers/api/local_plan_steps_controller.rb
@@ -1,9 +1,13 @@
 # controllers/api/local_plan_steps_controller.rb
+#
+# Controller responsible for handling API endpoints related to local plan steps.
+
 class Api::LocalPlanStepsController < ApplicationController
+  # Skips the authenticity token verification for API requests.
   skip_before_action :verify_authenticity_token
 
+  # Includes modules containing index-related actions, step actions, and private methods.
   include LocalPlanStepsIndex
   include LocalPlanStepsActions
   include LocalPlanStepsPrivate
-
 end

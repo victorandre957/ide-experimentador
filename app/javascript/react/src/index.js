9fc9235e6eefe335dd843b95b744cd14e58b1c96
Victor André <victorandre957@gmail.com>
2023-12-14 15:01:20 -0300

    Hello (fix): restore hello file

--- app/javascript/react/src/index.js
+++ app/javascript/react/src/index.js
@@ -1,6 +1,8 @@
 import { define } from 'remount'      
+import Hello from "./components/Hello"
 import Graph from "./components/Graph"
 import CreateLocalPlan from "./pages/CreateLocalPlan";
 import { wrapWithQueryProvider } from "./wrapWithQueryProvider";
                                       
+define({ 'hello-component': Hello, 'graph-component': Graph })
 define({ "create-local-plan-component": wrapWithQueryProvider(CreateLocalPlan) });

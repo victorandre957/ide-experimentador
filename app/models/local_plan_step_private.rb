5dd067985ac2e8c6c94f5cacbedfb9c10239907c
Victor André <victorandre957@gmail.com>
2023-12-17 11:35:27 -0300

    local_plan_step (fix): fix validations

--- app/models/local_plan_step_private.rb
+++ app/models/local_plan_step_private.rb
@@ -4,33 +4,35 @@
 module LocalPlanStepPrivate
   # Validates navigation parameters for a given record.
   # Expects the presence of "room" and "waypoints" keys in the parameter hash, with specified sizes.
-  def validate_navigation(record)
-    validate_parameter(record, %w(room waypoints), [2, 4])
-  end
+  def validate(record)
+    puts record.parameter
+    puts record.skill
+    case record.skill
+      when "navigation"
+        if  !(record.parameter.has_key?("room")) or !(record.parameter.has_key?("waypoints"))
+          record.errors.add :base, "Incorrect parameter formating"    
 
-  # Validates common parameters for a given record.
-  # Expects the presence of a "topic" key in the parameter hash, with a specified size.
-  def validate_common(record)
-    validate_parameter(record, ["topic"], [1])
-  end
+        elsif record.parameter.size != 2
+          record.errors.add :base, "Wrong number of characteristics" 
 
-  # Validates drawer parameters for a given record.
-  # Expects the presence of an "action" key in the parameter hash, with a specified size.
-  # Additionally, checks if the action command is either "open" or "close".
-  def validate_drawer(record)
-    validate_parameter(record, ["action"], [1])
-    valid_actions = %w(open close)
-    record.errors.add(:base, "Incorrect action command") unless valid_actions.include?(record.parameter["action"])
-  end
+        elsif record.parameter["waypoints"].size != 4
+          record.errors.add :base, "Incorrect number of waypoints" 
+        end
 
-  # Generic method for validating parameters based on provided keys and sizes.
-  # Adds errors to the record if the parameter formatting or characteristics are incorrect.
-  def validate_parameter(record, keys, sizes)
-    keys.each_with_index do |key, index|
-      unless record.parameter.has_key?(key) && record.parameter.size == sizes[index]
-        record.errors.add :base, "Incorrect parameter formatting or characteristics"
-        break
-      end
+      when "approach_robot", "authenticate_person", "approach_person", "wait_message", "send_message"
+        if  !(record.parameter.has_key?("topic")) or record.parameter.size != 1
+          record.errors.add :base, "Incorrect parameter formating"       
+        end
+
+      when "operate_drawer"   
+        if  !(record.parameter.has_key?("action")) or record.parameter.size != 1
+          record.errors.add :base, "Incorrect parameter formating"
+        elsif record.parameter["action"] != "open" or record.parameter["action"] != "close"
+          record.errors.add :base, "Incorrect action command"         
+        end
+
+      else
+        record.errors.add :base, "This skill is evil"
     end
   end
 end

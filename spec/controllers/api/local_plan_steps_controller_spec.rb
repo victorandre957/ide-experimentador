b8eaa2e7964f72db890646b439e8ae919d91c330
Henrixii <luizhfs09@gmail.com>
2023-12-18 10:09:46 -0300

    test: creating more tests for create

--- spec/controllers/api/local_plan_steps_controller_spec.rb
+++ spec/controllers/api/local_plan_steps_controller_spec.rb
@@ -21,20 +21,247 @@ describe Api::LocalPlanStepsController, type: :controller do
               ]
             }, skill:"navigation", robot_id: robot.id}
         end
-        context 'should create a plan with good params' do
+        context 'should create a plan with good params(skill: navigation)' do
             it 'return status created' do
                 post :create, params: {local_plan_step: local_plan_step_params}
                     expect(response).to have_http_status(:created)
-#                    expect { post :create, params: { local_plan_step: local_plan_step_params } }.to change(LocalPlanStep, :count).by(1)
+                  #  expect { post :create, params: { local_plan_step: local_plan_step_params } }.to change(LocalPlanStep, :count).by(1)
             end
         end
-        context 'should create a plan with bad params' do
-            it 'return status bad_request' do
-                local_plan_step_params[:stepNumber] = nil
+        context 'should create a plan with good params(skill: approach_robot)' do
+          it 'return status created' do
+            local_plan_step_params[:skill] = "approach_robot"
+            local_plan_step_params[:label] = "approach_arm"
+            local_plan_step_params[:parameter] = {topic: "lab_arm"}
+            post :create, params: {local_plan_step: local_plan_step_params}
+              expect(response).to have_http_status(:created)
+                
+          end
+      end
+        context 'should create a plan with good params(authentication_person)' do
+          it 'return status created' do
+            local_plan_step_params[:skill] = "authenticate_person"
+            local_plan_step_params[:label] = "authenticate_nurse"
+            local_plan_step_params[:parameter] = {topic: "nurse"}
+            post :create, params: {local_plan_step: local_plan_step_params}
+              expect(response).to have_http_status(:created)
+                
+          end
+      end
+        context 'should create a plan with good params(approuch_person)' do
+          it 'return status created' do
+            local_plan_step_params[:skill] = "approach_person"
+            local_plan_step_params[:label] = "approach_nurse"
+            local_plan_step_params[:parameter] = {topic: "nurse"}
+            post :create, params: {local_plan_step: local_plan_step_params}
+              expect(response).to have_http_status(:created)
+                
+          end
+      end
+        context 'should create a plan with good params(wait_message)' do
+          it 'return status created' do
+            local_plan_step_params[:skill] = "wait_message"
+            local_plan_step_params[:label] = "wait_nurse_to_complete_deposit"
+            local_plan_step_params[:parameter] = {topic: "nurse"}
+            post :create, params: {local_plan_step: local_plan_step_params}
+              expect(response).to have_http_status(:created)
+                
+          end
+      end
+        context 'should create a plan with good params(send_message)' do
+          it 'return status created' do
+            local_plan_step_params[:skill] = "send_message"
+            local_plan_step_params[:label] = "notify_lab_arm_of_open_drawer_lab_completed"
+            local_plan_step_params[:parameter] = {topic: "nurse"}
+            post :create, params: {local_plan_step: local_plan_step_params}
+              expect(response).to have_http_status(:created)
+                
+          end
+      end
+        context 'should create a plan with good params(operate_drawer)' do
+          it 'return status created' do
+            local_plan_step_params[:skill] = "operate_drawer"
+            local_plan_step_params[:label] = "open_drawer_for_nurse"
+            local_plan_step_params[:parameter] = {action: "open"}
+            post :create, params: {local_plan_step: local_plan_step_params}
+              expect(response).to have_http_status(:created)
+                
+          end
+      end
+        context 'should create a plan with good params(operate_drawer)' do
+          it 'return status created' do
+            local_plan_step_params[:skill] = "operate_drawer"
+            local_plan_step_params[:label] = "close_drawer_for_nurse"
+            local_plan_step_params[:parameter] = {action: "close"}
+            post :create, params: {local_plan_step: local_plan_step_params}
+              expect(response).to have_http_status(:created)
+                
+          end
+      end
+      
+        context 'should create a plan with invalid stepNumeber' do
+          it 'return status unprocessable_entity' do
+              local_plan_step_params[:stepNumber] = "non numeric"
                 post :create , params: {local_plan_step: local_plan_step_params}
+                    expect(response).to have_http_status(:unprocessable_entity)
+            end
+        end
+        context 'should create a plan with invalid label' do
+            it 'return status unprocessable_entity' do
+              local_plan_step_params[:label] = nil
+              post :create , params: {local_plan_step: local_plan_step_params}
                 expect(response).to have_http_status(:unprocessable_entity)
             end
         end
+        context 'should create a plan with invalid robot' do
+          it 'return status unprocessable_entity' do
+            local_plan_step_params[:robot_id] = [-1]
+            post :create , params: {local_plan_step: local_plan_step_params}
+            expect(response).to have_http_status(:unprocessable_entity)
+          end
+      end
+        context 'should create a plan with invalid skill' do
+          it 'return status unprocessable_entity' do
+            local_plan_step_params[:skill] = "invalid skill"
+            post :create , params: {local_plan_step: local_plan_step_params}
+            expect(response).to have_http_status(:unprocessable_entity)
+          end
+      end
+      #The next creat tests related to the parameters are using skill: operate_drawer
+        context 'should create a plan with invalid parameter name' do
+          it 'return status unprocessable_entity' do
+            local_plan_step_params[:parameter] = {invalid: "open"}
+            post :create , params: {local_plan_step: local_plan_step_params}
+            expect(response).to have_http_status(:unprocessable_entity)
+        end
+      end
+        context 'should create a plan with invalid parameter(size)' do
+          it 'return status unprocessable_entity' do
+            local_plan_step_params[:parameter] = {action:"open", invalid:"open"}
+            post :create , params: {local_plan_step: local_plan_step_params}
+            expect(response).to have_http_status(:unprocessable_entity)
+          end
+      end
+        context 'should create a plan with invalid parameter(action)' do
+          it 'return status unprocessable_entity' do
+            local_plan_step_params[:parameter] = {action: "invalid"}
+            post :create , params: {local_plan_step: local_plan_step_params}
+            expect(response).to have_http_status(:unprocessable_entity)
+        end
+      end
+        #The next creat tests related to the parameters are using skill: navigation
+        context 'should create a plan with invalid parameter name' do
+          it 'return status unprocessable_entity' do
+            local_plan_step_params[:parameter] = {invalid: "PC room 3", invalid: [
+              { x: -27.23, y: 18.0, z: -1.57 },
+              { x: -27.23, y: 16.0 },
+              { x: -28.5, y: 16.0 },
+              { x: -28.5, y: 18.0, z: -1.57 }]}
+            post :create , params: {local_plan_step: local_plan_step_params}
+            expect(response).to have_http_status(:unprocessable_entity)
+        end
+      end
+        context 'should create a plan with invalid parameter(size)' do
+          it 'return status unprocessable_entity' do
+            local_plan_step_params[:parameter] = {room:"invalid room"}
+            post :create , params: {local_plan_step: local_plan_step_params}
+            expect(response).to have_http_status(:unprocessable_entity)
+          end
+      end
+        context 'should create a plan with invalid parameter(waypoints)' do
+          it 'return status unprocessable_entity' do
+            local_plan_step_params[:parameter] = {room: "PC room 3", waypoints: [
+              { x: -27.23, y: 18.0, z: -1.57 },
+              { x: -27.23, y: 16.0 },
+              { x: -28.5, y: 16.0 },]}
+            post :create , params: {local_plan_step: local_plan_step_params}
+            expect(response).to have_http_status(:unprocessable_entity)
+        end
+      end
+        #The next creat tests related to the parameters are using skill: approach_robot
+        context 'should create a plan with invalid parameter name' do
+          it 'return status unprocessable_entity' do
+            local_plan_step_params[:parameter] = {invalid: "lab_arm"}
+            post :create , params: {local_plan_step: local_plan_step_params}
+            expect(response).to have_http_status(:unprocessable_entity)
+        end
+      end
+        context 'should create a plan with invalid parameter(size)' do
+          it 'return status unprocessable_entity' do
+            local_plan_step_params[:parameter] = {topic:"lab_arm", invalid:"test"}
+            post :create , params: {local_plan_step: local_plan_step_params}
+            expect(response).to have_http_status(:unprocessable_entity)
+          end
+      end
+        #The next creat tests related to the parameters are using skill: authenticate_person
+        context 'should create a plan with invalid parameter name' do
+          it 'return status unprocessable_entity' do
+            local_plan_step_params[:parameter] = {topic: "nurse"}
+            post :create , params: {local_plan_step: local_plan_step_params}
+            expect(response).to have_http_status(:unprocessable_entity)
+        end
+      end
+        context 'should create a plan with invalid parameter(size)' do
+          it 'return status unprocessable_entity' do
+            local_plan_step_params[:parameter] = {topic:"nurse", invalid:"test"}
+            post :create , params: {local_plan_step: local_plan_step_params}
+            expect(response).to have_http_status(:unprocessable_entity)
+          end
+      end
+        #The next creat tests related to the parameters are using skill: approach_person
+        context 'should create a plan with invalid parameter name' do
+          it 'return status unprocessable_entity' do
+            local_plan_step_params[:parameter] = {topic: "nurse"}
+            post :create , params: {local_plan_step: local_plan_step_params}
+            expect(response).to have_http_status(:unprocessable_entity)
+        end
+      end
+        context 'should create a plan with invalid parameter(size)' do
+          it 'return status unprocessable_entity' do
+            local_plan_step_params[:parameter] = {topic:"nurse", invalid:"test"}
+            post :create , params: {local_plan_step: local_plan_step_params}
+            expect(response).to have_http_status(:unprocessable_entity)
+          end
+      end
+        #The next creat tests related to the parameters are using skill: wait_message
+        context 'should create a plan with invalid parameter name' do
+          it 'return status unprocessable_entity' do
+            local_plan_step_params[:parameter] = {topic: "nurse"}
+            post :create , params: {local_plan_step: local_plan_step_params}
+            expect(response).to have_http_status(:unprocessable_entity)
+        end
+      end
+        context 'should create a plan with invalid parameter(size)' do
+          it 'return status unprocessable_entity' do
+            local_plan_step_params[:parameter] = {topic:"nurse", invalid:"test"}
+            post :create , params: {local_plan_step: local_plan_step_params}
+            expect(response).to have_http_status(:unprocessable_entity)
+          end
+      end
+        #The next creat tests related to the parameters are using skill: send_message
+        context 'should create a plan with invalid parameter name' do
+          it 'return status unprocessable_entity' do
+            local_plan_step_params[:parameter] = {topic: "nurse"}
+            post :create , params: {local_plan_step: local_plan_step_params}
+            expect(response).to have_http_status(:unprocessable_entity)
+        end
+      end
+        context 'should create a plan with invalid parameter(size)' do
+          it 'return status unprocessable_entity' do
+            local_plan_step_params[:parameter] = {topic:"nurse", invalid:"test"}
+            post :create , params: {local_plan_step: local_plan_step_params}
+            expect(response).to have_http_status(:unprocessable_entity)
+          end
+      end
+        
+
+
+
+
+
+
+
+
     end
     describe 'DELETE #delete' do
     let!(:robot) {Robot.create!()}

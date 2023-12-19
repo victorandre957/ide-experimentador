61a2b38a62c0789a8bee8664fa0a40bc5cfa5470
artistrea <apiravesi@gmail.com>
2023-12-19 12:48:10 -0300

    feat: ok ui and display error on frontend

--- app/javascript/react/src/pages/CreateLocalPlan.jsx
+++ app/javascript/react/src/pages/CreateLocalPlan.jsx
@@ -2,13 +2,14 @@ import React, { useState } from "react";
 import LocalPlanForm from "../components/LocalPlanForm";
 import styled from "styled-components";
 import axios from "axios";
+import toast, { Toaster } from "react-hot-toast";
 
 let existPlan = false;
 
-const CreateLocalPlan = ({ robotId }) => {
+const CreateLocalPlan = ({}) => {
   const [steps, setSteps] = useState([]);
 
-  let robotId = 999;
+  let robotId = 1;
 
   if (false) {
     useEffect(() => {
@@ -53,59 +54,155 @@ const CreateLocalPlan = ({ robotId }) => {
     const url = existPlan
       ? `/api/local_plan_steps/${robotId}`
       : "/api/local_plan_steps";
-    
-    let robotId = 999
-    
+
+    let fetchPromise;
     if (!existPlan) {
-      axios
-        .post(url, { local_plan: steps, robot_Id: robotId })
-        .then((response) => {
-          console.log("Passos salvos/atualizados com sucesso:", response.data);
-        })
-        .catch((error) => {
-          console.error("Erro ao salvar/atualizar os passos:", error);
-        });
+      fetchPromise = axios.post(url, { local_plan: steps, robot_Id: robotId });
+
+      toast.promise(fetchPromise, {
+        loading: "Criando Plano Local",
+        success: "Plano Local criado com sucesso",
+        error: (err) => err.response.data.message,
+      });
     } else {
-      axios
-        .update(url, { local_plan: steps, robot_Id: robotId })
-        .then((response) => {
-          console.log("Passos salvos/atualizados com sucesso:", response.data);
-        })
-        .catch((error) => {
-          console.error("Erro ao salvar/atualizar os passos:", error);
-        });
+      fetchPromise = axios.update(url, {
+        local_plan: steps,
+        robot_Id: robotId,
+      });
+
+      toast.promise(fetchPromise, {
+        loading: "Atualizando o Plano Local",
+        success: "Plano Local atualizado com sucesso",
+        error: (err) => err.response.data.message,
+      });
     }
   };
 
-  const TitleDiv = styled.div`
-    display: flex;
-    flex-direction: row;
-    justify-content: space-between;
-    padding: 0 1rem;
-  `;
-  const ContentDiv = styled.div`
-    display: grid;
-    grid-template-columns: 1fr;
-    gap: 1rem;
-  `;
   return (
-    <div>
-      <div>
-        <h1>Criar uma sequência de passos</h1>
-        <button onClick={handleDeletePlan}>Deletar Plano Local</button>
+    <>
+      <Toaster
+        toastOptions={{
+          success: {
+            className: "bg-green-100 p-5 rounded",
+            icon: (
+              <span className="flex">
+                <svg
+                  xmlns="http://www.w3.org/2000/svg"
+                  width="24"
+                  height="24"
+                  viewBox="0 0 24 24"
+                  fill="none"
+                  stroke="currentColor"
+                  strokeWidth="2"
+                  strokeLinecap="round"
+                  strokeLinejoin="round"
+                  className="text-green-500"
+                >
+                  <path d="M20 6 9 17l-5-5" />
+                </svg>
+                Sucesso
+              </span>
+            ),
+          },
+          error: {
+            className: "bg-red-100 p-5 rounded",
+            icon: (
+              <span className="flex">
+                <svg
+                  xmlns="http://www.w3.org/2000/svg"
+                  width="24"
+                  height="24"
+                  viewBox="0 0 24 24"
+                  fill="none"
+                  stroke="currentColor"
+                  strokeWidth="2"
+                  strokeLinecap="round"
+                  strokeLinejoin="round"
+                  className="text-red-500"
+                >
+                  <path d="M18 6 6 18" />
+                  <path d="m6 6 12 12" />
+                </svg>
+                Algo deu errado
+              </span>
+            ),
+          },
+        }}
+      />
+      <div className="grid grid-cols-2 p-16">
+        <div>
+          <h1 className="text-3xl font-bold tracking-tight">
+            Crie uma sequência de passos
+          </h1>
+          <div className="flex flex-col items-center w-full">
+            <div className="w-full">
+              {steps.map((step, index) => (
+                <LocalPlanForm
+                  key={index} // Adicione a chave única para cada Input
+                  step={step}
+                  stepIndex={index}
+                  onChange={handleChange}
+                  onRemove={handleRemoveStep}
+                />
+              ))}
+            </div>
+            <button
+              className="rounded px-4 py-3 my-4 bg-zinc-100"
+              onClick={handleAddStep}
+            >
+              Adicionar Novo Passo
+            </button>
+            <button
+              className="rounded text-green-700 bg-green-100 px-4 py-3 flex gap-2 mr-auto mt-10"
+              onClick={handleSave}
+            >
+              <svg
+                xmlns="http://www.w3.org/2000/svg"
+                width="24"
+                height="24"
+                viewBox="0 0 24 24"
+                fill="none"
+                stroke="currentColor"
+                strokeWidth="2"
+                strokeLinecap="round"
+                strokeLinejoin="round"
+              >
+                <path d="M19 21H5a2 2 0 0 1-2-2V5a2 2 0 0 1 2-2h11l5 5v11a2 2 0 0 1-2 2z" />
+                <polyline points="17 21 17 13 7 13 7 21" />
+                <polyline points="7 3 7 8 15 8" />
+              </svg>
+              Salvar Plano Local
+            </button>
+          </div>
+        </div>
+
+        <div className="flex justify-end items-start">
+          <button
+            className="rounded text-red-700 bg-red-100 px-4 py-3 flex gap-2"
+            onClick={handleDeletePlan}
+          >
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              width="24"
+              height="24"
+              viewBox="0 0 24 24"
+              fill="none"
+              stroke="currentColor"
+              strokeWidth="2"
+              strokeLinecap="round"
+              strokeLinejoin="round"
+            >
+              <path d="M3 6h18" />
+              <path d="M19 6v14c0 1-1 2-2 2H7c-1 0-2-1-2-2V6" />
+              <path d="M8 6V4c0-1 1-2 2-2h4c1 0 2 1 2 2v2" />
+              <line x1="10" x2="10" y1="11" y2="17" />
+              <line x1="14" x2="14" y1="11" y2="17" />
+            </svg>
+            Deletar Plano Local
+          </button>
+        </div>
       </div>
-      {steps.map((step, index) => (
-        <LocalPlanForm
-          key={index} // Adicione a chave única para cada Input
-          step={step}
-          stepIndex={index}
-          onChange={handleChange}
-          onRemove={handleRemoveStep}
-        />
-      ))}
-      <button onClick={handleAddStep}>Adicionar Passo</button>
-      <button onClick={handleSave}>Salvar</button>
-    </div>
+    </>
   );
 };
 

0ef4ef6cdd30b9657a7f8423a7ab864094b84e35
Victor André <victorandre957@gmail.com>
2023-12-19 17:49:32 -0300

    (docs): generete rdoc documentation

--- app/controllers/api/local_plan_steps_controller.rb
+++ app/controllers/api/local_plan_steps_controller.rb
@@ -1,16 +1,29 @@
+# Controller responsible for managing LocalPlanSteps associated with a Robot in the API.
 class Api::LocalPlanStepsController < ApplicationController
+  # Skips authenticity token verification for actions within this controller.
   skip_before_action :verify_authenticity_token
 
+  # Retrieves all local plan steps associated with a specific robot, ordered by step number.
+  #
+  # GET /api/local_plan_steps/index_by_robot
+  # @param params[:robot_id] [String] The ID of the robot to retrieve steps for.
+  # @return [JSON] Returns a JSON response containing the retrieved steps.
   def index_by_robot
+    # Extracts the robot_id from parameters
     robot_id = params[:robot_id]
-
+    # Fetches steps associated with the given robot and orders them by step number
     steps = LocalPlanStep
       .where(robot_id: robot_id)
       .order(:stepNumber)
-
+    # Renders the steps as JSON with a successful status
     render json: steps, status: :ok
   end
-
+  # Creates local plan steps for a specific robot.
+  #
+  # POST /api/local_plan_steps/create
+  # @param params[:local_plan] [Array<Hash>] Array of parameters for creating local plan steps.
+  # @param params[:robot_Id] [String] The ID of the robot to associate the steps with.
+  # @return [JSON] Returns a JSON response containing the created steps.
   def create
     local_plan_steps = params.require(:local_plan)
     robot_id = params.require(:robot_Id)
@@ -33,6 +46,12 @@ class Api::LocalPlanStepsController < ApplicationController
     render json: { message: e.message }, status: 422
   end
 
+  # Updates local plan steps associated with a specific robot.
+  #
+  # PUT /api/local_plan_steps/update
+  # @param params[:local_plan] [Array<Hash>] Array of parameters for updating local plan steps.
+  # @param params[:robot_Id] [String] The ID of the robot whose steps are being updated.
+  # @return [JSON] Returns a JSON response indicating successful step updates.
   def update
     robot_id = params.require(:robot_Id)
     local_plan_steps = params.require(:local_plan)
@@ -60,6 +79,11 @@ class Api::LocalPlanStepsController < ApplicationController
       render json: { message: e.message }, status: 404
   end
 
+  # Deletes all local plan steps associated with a specific robot.
+  #
+  # DELETE /api/local_plan_steps/delete
+  # @param params[:robot_id] [String] The ID of the robot whose steps are to be deleted.
+  # @return [JSON] Returns a JSON response indicating successful deletion of steps.
   def delete
     robot_id = params[:robot_id]
     deleted_steps = LocalPlanStep.where(robot_id: robot_id).destroy_all
@@ -68,6 +92,12 @@ class Api::LocalPlanStepsController < ApplicationController
     render json: { message: e.message }, status: 404
   end
 
+  # Deletes a specific local plan step.
+  #
+  # DELETE /api/local_plan_steps/delete_step
+  # @param params[:id] [String] The ID of the step to be deleted.
+  # @return [JSON] Returns a JSON response indicating successful deletion of the step.
+  
   def delete_step
     if (params[:id])
       render json: LocalPlanStep.destroy(params[:id]), status: :ok
@@ -78,6 +108,10 @@ class Api::LocalPlanStepsController < ApplicationController
   
   private
 
+  # Defines permitted parameters for creating/updating local plan steps based on skill types.
+  #
+  # @param step_params [Hash] Parameters for a local plan step.
+  # @return [ActionController::Parameters] Permitted parameters for creating/updating steps.
   def local_plan_step_params(step_params)
     skill = step_params[:skill]
   
